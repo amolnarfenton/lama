ca6e3593a033fadde16e65b3d61b88c5d7276f04
Caitlyn Gemma <caitlyngemma@Caitlyns-MacBook-Pro.local>
2015-04-18 12:22:25 -0400

    Adding Stress Quiz Folder

---
+++ Stress Quiz/question.js
@@ -0,0 +1,51 @@
+var Questions = { "question" : [
+  {
+    "question"  : "How are you feeling today (1=bad, 5=AWESOME!)?",
+     "choice1"  : "1",
+     "choice2"  : "2",
+     "choice3"  : "3",
+     "choice4"  : "4",
+     "choice5"  : "5",                    
+     "correct"  : 1
+  }/*,
+  
+  {
+    "question"  : "What creature does Dumbledore have as a pet?",
+     "choice1"  : "Efreet",
+     "choice2"  : "Fey",
+     "choice3"  : "Troll",
+     "choice4"  : "Phoenix",
+     "choice5"  : "Basilisk",                    
+     "correct"  : 4
+  },
+  
+  {
+    "question"  : "What is Voldemort's final horcrux?",
+     "choice1"  : "A mirror",
+     "choice2"  : "A snake",
+     "choice3"  : "A brooch",
+     "choice4"  : "Harry Potter",
+     "choice5"  : "A violin",                    
+     "correct"  : 2
+  },
+  
+  {
+    "question"  : "Who takes over as headmaster of Hogwarts after Dumbledore's death?",
+     "choice1"  : "Voldemort",
+     "choice2"  : "Narcissa Black",
+     "choice3"  : "Professor Trelawny",
+     "choice4"  : "Delores Umbridge",
+     "choice5"  : "Professor Snape",                    
+     "correct"  : 5
+  },
+  
+  {
+    "question"  : "Who killed Deatheater Antonin Dolohov during the Battle of Hogwarts?",
+     "choice1"  : "Professor Flitwick",
+     "choice2"  : "Ron Weasley",
+     "choice3"  : "Falling Debris",
+     "choice4"  : "Hermione Granger",
+     "choice5"  : "A Troll",                    
+     "correct"  : 1
+  }*/
+]};
