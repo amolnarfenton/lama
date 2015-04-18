51c8b1a3b3335be5ab72d45eb0a9d1cafe4c8d11
Meredith Clark <meredithclark@Merediths-MacBook-Pro.local>
2015-04-18 19:03:42 -0400

    Adding questions

--- Stress Quiz/question.js
+++ Stress Quiz/question.js
@@ -1,42 +1,40 @@
 var Questions = { "question" : [
   {
-    "question"  : "How are you feeling today (1=bad, 5=AWESOME!)?",
-     "choice1"  : "1",
-     "choice2"  : "2",
-     "choice3"  : "3",
-     "choice4"  : "4",
-     "choice5"  : "5",                    
-     "correct"  : 1
-  }/*,
-  
-  {
-    "question"  : "What creature does Dumbledore have as a pet?",
-     "choice1"  : "Efreet",
-     "choice2"  : "Fey",
-     "choice3"  : "Troll",
-     "choice4"  : "Phoenix",
-     "choice5"  : "Basilisk",                    
-     "correct"  : 4
+    "question"  : "On average, how many hours do you sleep per night?",
+     "choice1"  : "< 6",
+     "choice2"  : "6-10",
+     "choice3"  : "> 10",
+     "tooLittle": 1,
+     "healthy"  : 2,
+     "tooMuch"  : 3            
   },
   
   {
-    "question"  : "What is Voldemort's final horcrux?",
-     "choice1"  : "A mirror",
-     "choice2"  : "A snake",
-     "choice3"  : "A brooch",
-     "choice4"  : "Harry Potter",
-     "choice5"  : "A violin",                    
-     "correct"  : 2
+    "question"  : "How often do you exercise in a typical week?",
+     "choice1"  : "Never",
+     "choice2"  : "1-5 days",
+     "choice3"  : "6+",
+     "tooLittle": 1,
+     "healthy"  : 2,
+     "tooMuch"  : 3            
   },
   
+  {
+    "question"  : "How much personal time do you take per day, in hours?",
+     "choice1"  : "0-4",
+     "choice2"  : "4-8",
+     "choice3"  : "8+",
+     "tooLittle": 1,
+     "healthy"  : 2,
+     "tooMuch"  : 3                 
+  },
+  /*
   {
     "question"  : "Who takes over as headmaster of Hogwarts after Dumbledore's death?",
      "choice1"  : "Voldemort",
      "choice2"  : "Narcissa Black",
      "choice3"  : "Professor Trelawny",
-     "choice4"  : "Delores Umbridge",
-     "choice5"  : "Professor Snape",                    
-     "correct"  : 5
+              
   },
   
   {
@@ -44,8 +42,6 @@ var Questions = { "question" : [
      "choice1"  : "Professor Flitwick",
      "choice2"  : "Ron Weasley",
      "choice3"  : "Falling Debris",
-     "choice4"  : "Hermione Granger",
-     "choice5"  : "A Troll",                    
-     "correct"  : 1
+            
   }*/
 ]};
