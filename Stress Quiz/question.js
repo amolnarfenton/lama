3580b71b3fb8a9554c7451d9c00bb49f10f0ff8c
Caitlyn Gemma <caitlyngemma@Caitlyns-MacBook-Pro.local>
2015-04-19 00:29:33 -0400

    updating from the bad merge

--- Stress Quiz/question.js
+++ Stress Quiz/question.js
@@ -1,15 +1,5 @@
 var Questions = { "question" : [
   {
-<<<<<<< HEAD
-    "question"  : "How are you feeling today (1=bad, 5=AWESOME!)?",
-     "choice1"  : "1",
-     "choice2"  : "2",
-     "choice3"  : "3",
-     "choice4"  : "4",
-     "choice5"  : "5",                    
-     "correct"  : 1
-  }
-=======
     "question"  : "On average, how many hours do you sleep per night?",
      "choice1"  : "< 6",
      "choice2"  : "6-10",
@@ -37,21 +27,5 @@ var Questions = { "question" : [
      "average"  : 2,
      "aboveAvg" : 3                           
   }
-  /*
-  {
-    "question"  : "Who takes over as headmaster of Hogwarts after Dumbledore's death?",
-     "choice1"  : "Voldemort",
-     "choice2"  : "Narcissa Black",
-     "choice3"  : "Professor Trelawny",
-              
-  },
-  
-  {
-    "question"  : "Who killed Deatheater Antonin Dolohov during the Battle of Hogwarts?",
-     "choice1"  : "Professor Flitwick",
-     "choice2"  : "Ron Weasley",
-     "choice3"  : "Falling Debris",
-            
-  }*/
->>>>>>> 33b3e2c43e64be0c0e61d92f60b93cff987a8265
+
 ]};
